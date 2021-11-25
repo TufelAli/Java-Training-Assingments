bab3da0e37fa89921a3b11f935fca288db2bc2f1
Tufel Ali <tufelq6.998@gmail.com>
2021-11-26 02:57:48 +0530

    Add files via upload

---
+++ Java Day 3/Assignment2/src/tufel/AgeException.java
@@ -0,0 +1,8 @@
+package tufel;
+
+public class AgeException extends /*RuntimeException*/Exception {
+
+	public AgeException(String message) {
+		super(message);
+	}
+}
---
+++ Java Day 3/Assignment2/src/tufel/NameException.java
@@ -0,0 +1,9 @@
+package tufel;
+
+public class NameException extends /*RuntimeException*/Exception {
+
+	public NameException(String message) {
+		super(message);
+	}
+}
+
