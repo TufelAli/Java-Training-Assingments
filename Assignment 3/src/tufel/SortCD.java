b041ad8957d468d21832934c7c835f6fa04e9499
Tufel Ali <tufelq6.998@gmail.com>
2021-11-27 16:57:47 +0530

    Add files via upload

---
+++ Assignment 3/src/tufel/SortCD.java
@@ -0,0 +1,13 @@
+package tufel;
+
+import java.util.Comparator;
+
+public class SortCD implements Comparator<CD> {
+
+	@Override
+	public int compare(CD cd1, CD cd2) {
+		
+		return cd1.getSinger().compareTo(cd2.getSinger());
+	}
+
+}
